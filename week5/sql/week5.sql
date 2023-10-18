9c5e4d974143562bbcab8cb2ed4ecd9d641d47d3
Nguyen Dinh Tran Long <longndt226@gmail.com>
2023-10-18 09:54:23 +0700

    web

---
+++ week5/sql/week5.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 18, 2023 at 04:53 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `week5`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `joke`
+--
+
+CREATE TABLE `joke` (
+  `id` int(11) NOT NULL,
+  `joketext` text NOT NULL,
+  `jokedate` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `joke`
+--
+
+INSERT INTO `joke` (`id`, `joketext`, `jokedate`) VALUES
+(1, 'joke 1', '2023-10-02'),
+(3, 'joke 3', '2023-10-16'),
+(4, 'this is a new joke\r\n', '2023-10-16');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `joke`
+--
+ALTER TABLE `joke`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `joke`
+--
+ALTER TABLE `joke`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

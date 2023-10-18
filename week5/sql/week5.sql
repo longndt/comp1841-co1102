7764e77e719555a93755ae943d898b349f938857
Nguyen Dinh Tran Long <longndt226@gmail.com>
2023-10-18 10:49:57 +0700

    web

--- week5/sql/week5.sql
+++ week5/sql/week5.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 18, 2023 at 04:53 AM
+-- Generation Time: Oct 18, 2023 at 05:49 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `week5`
+-- Database: `week4`
 --
 
 -- --------------------------------------------------------
@@ -30,17 +30,31 @@ SET time_zone = "+00:00";
 CREATE TABLE `joke` (
   `id` int(11) NOT NULL,
   `joketext` text NOT NULL,
-  `jokedate` date NOT NULL
+  `jokedate` date NOT NULL,
+  `image` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `joke`
 --
 
-INSERT INTO `joke` (`id`, `joketext`, `jokedate`) VALUES
-(1, 'joke 1', '2023-10-02'),
-(3, 'joke 3', '2023-10-16'),
-(4, 'this is a new joke\r\n', '2023-10-16');
+INSERT INTO `joke` (`id`, `joketext`, `jokedate`, `image`) VALUES
+(1, 'matt joke 1', '2023-10-02', 'chicken.png'),
+(3, 'my next joke', '2023-10-16', 'doctor.png'),
+(4, 'some chicken joke', '2023-10-24', 'knocker.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `joke_pic`
+--
+
+CREATE TABLE `joke_pic` (
+  `id` int(11) NOT NULL,
+  `jokedate` date NOT NULL,
+  `image` varchar(20) NOT NULL,
+  `pic` blob NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Indexes for dumped tables
@@ -53,6 +67,12 @@ ALTER TABLE `joke`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `joke_pic`
+--
+ALTER TABLE `joke_pic`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -61,6 +81,12 @@ ALTER TABLE `joke`
 --
 ALTER TABLE `joke`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `joke_pic`
+--
+ALTER TABLE `joke_pic`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;

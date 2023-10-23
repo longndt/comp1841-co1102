abcb95c0ed925beae28a69af8ed7e3ac03d31c22
Nguyen Dinh Tran Long <longndt226@gmail.com>
2023-10-23 09:08:55 +0700

    web

--- week6/sql/week6.sql
+++ week6/sql/week6.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 19, 2023 at 09:57 AM
+-- Generation Time: Oct 19, 2023 at 02:39 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -24,88 +24,43 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `author`
---
-
-CREATE TABLE `author` (
-  `id` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `author`
---
-
-INSERT INTO `author` (`id`, `name`, `email`) VALUES
-(1, 'author 1', 'author1@fpt.edu.vn'),
-(2, 'author 2', 'author2@fpt.edu.vn'),
-(3, 'author 3', 'author3@fpt.edu.vn\r\n');
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `joke`
 --
 
 CREATE TABLE `joke` (
   `id` int(11) NOT NULL,
   `joketext` text NOT NULL,
-  `jokedate` date NOT NULL,
-  `authorid` int(11) NOT NULL
+  `jokedate` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `joke`
 --
 
-INSERT INTO `joke` (`id`, `joketext`, `jokedate`, `authorid`) VALUES
-(16, 'joke 1', '2023-10-19', 1),
-(17, 'joke 2', '2023-10-08', 2),
-(18, 'joke 3', '2023-10-21', 3);
+INSERT INTO `joke` (`id`, `joketext`, `jokedate`) VALUES
+(1, 'joke 1', '2023-10-04'),
+(2, 'joke 2', '2023-10-07'),
+(3, 'joke 3', '2023-10-10');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `author`
---
-ALTER TABLE `author`
-  ADD PRIMARY KEY (`id`);
-
---
 -- Indexes for table `joke`
 --
 ALTER TABLE `joke`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `authorid` (`authorid`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `author`
---
-ALTER TABLE `author`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
 -- AUTO_INCREMENT for table `joke`
 --
 ALTER TABLE `joke`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `joke`
---
-ALTER TABLE `joke`
-  ADD CONSTRAINT `joke_ibfk_1` FOREIGN KEY (`authorid`) REFERENCES `author` (`id`);
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;

f6e2061d02605c2f4ac69171061f87aa0e699450
Nguyen Dinh Tran Long <longndt226@gmail.com>
2023-11-15 10:42:40 +0700

    web

---
+++ week9/addjoke.php
@@ -0,0 +1,25 @@
+<?php
+if(isset($_POST['joketext'])){
+    try{
+        include 'includes/DatabaseConnection.php';
+        include 'includes/DatabaseFunctions.php';
+
+        insertJoke($pdo, $_POST['joketext'],$_POST['authors'],$_POST['categories']);
+
+        header('location: jokes.php');
+    }catch (PDOException $e){
+        $title = 'An error has occurred';
+        $output = 'Database error: ' . $e->getMessage();
+    }
+}else{
+    include 'includes/DatabaseConnection.php';
+    include 'includes/DatabaseFunctions.php';
+    $title = 'Add a new joke';
+    $authors = allAuthors($pdo);
+    $categories = allCategories($pdo);
+    ob_start();
+    include 'templates/addjoke.html.php';
+    $output = ob_get_clean();
+}
+include 'templates/layout.html.php';
+
---
+++ week9/deletejoke.php
@@ -0,0 +1,12 @@
+<?php
+try{
+    include 'includes/DatabaseConnection.php';
+    include 'includes/DatabaseFunctions.php';
+    deleteJoke($pdo, $_POST['id']);
+    header('location: jokes.php');
+}catch(PDOException $e){
+$title = 'An error has occured';
+$output = 'Unable to delete joke: ' .$e->getMessage();
+}
+include 'templates/layout.html.php';
+
